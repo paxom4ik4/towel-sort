378494a25829dfaa6eac1833dbdcdfadc8d6f4db
Pavel Zelenko <43108877+paxom4ik4@users.noreply.github.com>
2020-03-06 22:38:46 +0300

    feat: change code

--- src/index.js
+++ src/index.js
@@ -2,15 +2,13 @@ module.exports = function towelSort(matrix) {
     if (!Array.isArray(matrix) || matrix.length === 0) {
         return [];
     }
-    let arr = []
+    const arr = []
     for (let i = 0; i < matrix.length; i++) {
         let value = [...matrix[i]];
         if (i % 2) {
             value.reverse();
         }
-        else{
             arr.push(...value);
-        }
     }
     return arr;
 }
